cec87425080e4c77da6087e191b4400b018a391f
Mike <michelsanchezaldana1@gmail.com>
2026-04-07 18:43:19 -0400

    Add test for 403 response when non-participant accesses messages

--- test/test_messaging_routes.py
+++ test/test_messaging_routes.py
@@ -34,7 +34,22 @@ class MessagingRoutesTests(unittest.TestCase):
         app.dependency_overrides[messaging_routes.get_current_user] = lambda: self.user
         self.client = TestClient(app)
 
-    
+    def tearDown(self):
+        self.client.close()
+
+    def test_get_messages_returns_403_for_non_participant(self):
+        with patch.object(
+            messaging_routes,
+            "verify_user_in_conversation",
+            return_value=False,
+        ):
+            response = self.client.get("/api/messaging/conversations/conv-1/messages")
+
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(
+            response.json()["detail"],
+            "You are not a participant in this conversation",
+        )
    
         
 if __name__ == "__main__":
