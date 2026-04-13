ced88ae6773510e4370003328c2076f6e205c892
Mike <michelsanchezaldana1@gmail.com>
2026-04-13 02:54:16 -0400

    add service test configuration and scripts

--- apps/expo-frontend/babel.config.js
+++ apps/expo-frontend/babel.config.js
@@ -1,6 +1,26 @@
 module.exports = function (api) {
   api.cache(true);
+
+  try {
+    require.resolve("babel-preset-expo");
+    return {
+      presets: ["babel-preset-expo"],
+    };
+  } catch (error) {
+    if (!api.env("test")) {
+      throw error;
+    }
+  }
+
   return {
-    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "@babel/plugin-transform-typescript",
+        {
+          allowDeclareFields: true,
+        },
+      ],
+      "@babel/plugin-transform-modules-commonjs",
+    ],
   };
 };
